98dd3a5ef16aaa04a240b1e12bf441acbe3fb37d
tom zhou <zs68j2ee@gmail.com>
2014-10-30 23:07:35 +0800

    added tweetnacl.c bench

--- tweetnacl/tweetnacl.c
+++ tweetnacl/tweetnacl.c
@@ -10,10 +10,10 @@ typedef i64 gf[16];
 extern void randombytes(u8 *,u64);
 
 static const u8
-  _0[16],
+  _0[16] = {0},
   _9[32] = {9};
 static const gf
-  gf0,
+  gf0 = {0},
   gf1 = {1},
   _121665 = {0xDB41,1},
   D = {0x78a3, 0x1359, 0x4dca, 0x75eb, 0xd8ab, 0x4141, 0x0a4d, 0x0070, 0xe898, 0x7779, 0x4079, 0x8cc7, 0xfe73, 0x2b6f, 0x6cee, 0x5203},
@@ -120,7 +120,7 @@ int crypto_core_hsalsa20(u8 *out,const u8 *in,const u8 *k,const u8 *c)
   return 0;
 }
 
-static const u8 sigma[16] = "expand 32-byte k";
+static const u8 sigma[17] = "expand 32-byte k";
 
 int crypto_stream_salsa20_xor(u8 *c,const u8 *m,u64 b,const u8 *n,const u8 *k)
 {
--- tweetnacl/tweetnacl.h
+++ tweetnacl/tweetnacl.h
@@ -1,5 +1,10 @@
 #ifndef TWEETNACL_H
 #define TWEETNACL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define crypto_auth_PRIMITIVE "hmacsha512256"
 #define crypto_auth crypto_auth_hmacsha512256
 #define crypto_auth_verify crypto_auth_hmacsha512256_verify
@@ -269,4 +274,9 @@ extern int crypto_verify_32_tweet(const unsigned char *,const unsigned char *);
 #define crypto_verify_32_BYTES crypto_verify_32_tweet_BYTES
 #define crypto_verify_32_VERSION crypto_verify_32_tweet_VERSION
 #define crypto_verify_32_IMPLEMENTATION "crypto_verify/32/tweet"
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
