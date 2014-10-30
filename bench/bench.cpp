2ce4b5b3d0bfaed9281ab43f60e952878ceea6e9
tom zhou <zs68j2ee@gmail.com>
2014-10-30 23:29:07 +0800

    fixed bench.cpp

--- bench/bench.cpp
+++ bench/bench.cpp
@@ -41,23 +41,23 @@ int main(int argc, char *argv[]) {
 		printf("\n\n\tstreess/%fkB: %d times\n", m0.length()/1000.0, t);
 
 		gettimeofday(&curTime, NULL);
-		printf("secret box ...@%6.3f\n", curTime.tv_sec*1000 + (curTime.tv_usec / 1000));
+		printf("secret box ...@%6.3f\n", curTime.tv_sec*1000.0 + (curTime.tv_usec / 1000.0));
 
 		unsigned char cab[crypto_secretbox_ZEROBYTES+m0.length()];
 		crypto_secretbox((unsigned char *)cab, (unsigned char *)mb0, m0.length(), (unsigned char *)nonce, (unsigned char *)shk);
 
 		gettimeofday(&curTime, NULL);
-		printf("... secret box@%6.3f\n", curTime.tv_sec*1000 + (curTime.tv_usec / 1000));
+		printf("... secret box@%6.3f\n", curTime.tv_sec*1000.0 + (curTime.tv_usec / 1000.0));
 
 
 		gettimeofday(&curTime, NULL);
-		printf("\nsecret box open ...@%f\n", curTime.tv_sec*1000 + (curTime.tv_usec / 1000));
+		printf("\nsecret box open ...@%6.3f\n", curTime.tv_sec*1000.0 + (curTime.tv_usec / 1000.0));
 
 		unsigned char mba[crypto_secretbox_ZEROBYTES+m0.length()];
 		crypto_secretbox_open((unsigned char *)mba, (unsigned char *)(cab+16), 16+m0.length(), (unsigned char *)nonce, (unsigned char *)shk);
 
 		gettimeofday(&curTime, NULL);
-		printf("... secret box open@%6.3f\n", curTime.tv_sec*1000 + (curTime.tv_usec / 1000));
+		printf("... secret box open@%6.3f\n", curTime.tv_sec*1000.0 + (curTime.tv_usec / 1000.0));
 	}
 
 	return 0;
